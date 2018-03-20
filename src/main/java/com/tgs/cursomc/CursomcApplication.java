a59f152cfa7f9025d8b8dc7c7bbb560405a37812
tiiiagoo <tiiiagoogs@gmail.com>
2018-03-20 12:00:55 -0300

    Produto e associação com categoria

--- src/main/java/com/tgs/cursomc/CursomcApplication.java
+++ src/main/java/com/tgs/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tgs.cursomc.domain.Categoria;
+import com.tgs.cursomc.domain.Produto;
 import com.tgs.cursomc.repositories.CategoriaRepository;
+import com.tgs.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository   produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,8 +27,21 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria c1 = new Categoria(null, "Informatica");
-		Categoria c2 = new Categoria(null, "Escritorio");		 	
+		Categoria c2 = new Categoria(null, "Escritorio");	
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null,"Mouse" , 80.00);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
 		
 		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 }
---
+++ src/main/java/com/tgs/cursomc/resources/ProdutoResource.java
@@ -0,0 +1,26 @@
+package com.tgs.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tgs.cursomc.domain.Produto;
+import com.tgs.cursomc.services.ProdutoService;
+
+@RestController
+@RequestMapping(value="/produtos")
+public class ProdutoResource {
+	
+	@Autowired
+	private ProdutoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?>  find(@PathVariable Long id) {
+		Produto produto = service.buscar(id);
+		return ResponseEntity.ok(produto);
+	}
+
+}
