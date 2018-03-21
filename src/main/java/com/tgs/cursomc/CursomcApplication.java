787ce7b58cd35b7458931effc90ca1259576b0fd
tiiiagoo <tiiiagoogs@gmail.com>
2018-03-21 00:43:12 -0300

    Cliente, TipoCliente, Telefones e Endereços

--- src/main/java/com/tgs/cursomc/CursomcApplication.java
+++ src/main/java/com/tgs/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tgs.cursomc.domain.Categoria;
 import com.tgs.cursomc.domain.Cidade;
+import com.tgs.cursomc.domain.Cliente;
+import com.tgs.cursomc.domain.Endereco;
 import com.tgs.cursomc.domain.Estado;
 import com.tgs.cursomc.domain.Produto;
+import com.tgs.cursomc.domain.enums.TipoCliente;
 import com.tgs.cursomc.repositories.CategoriaRepository;
 import com.tgs.cursomc.repositories.CidadeRepository;
+import com.tgs.cursomc.repositories.ClienteRepository;
+import com.tgs.cursomc.repositories.EnderecoRepository;
 import com.tgs.cursomc.repositories.EstadoRepository;
 import com.tgs.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository    estadoRepository;
 	@Autowired
 	private CidadeRepository    cidadeRepository;
+	@Autowired
+	private ClienteRepository   clienteRepository;
+	@Autowired
+	private EnderecoRepository  enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -56,11 +65,21 @@ public class CursomcApplication implements CommandLineRunner {
 		Cidade c3 = new Cidade(null, "Campinas", e2);	
 		
 		e1.getCidades().addAll(Arrays.asList(c1));
-		e2.getCidades().addAll(Arrays.asList(c2, c3));		
+		e2.getCidades().addAll(Arrays.asList(c2, c3));	
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));		
+		
+		Endereco end1 = new Endereco(null, "Rua Flores", "300", "apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco end2 = new Endereco(null, "Av Matos", "105", "sala 800", "Centro", "38777012", cli1, c1);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(e1,e2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(end1, end2));
 	}
 }
---
+++ src/main/java/com/tgs/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,38 @@
+package com.tgs.cursomc.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOA_FISICA(1,  "Pessoa Fisica"),
+	PESSOA_JURIDICA(2,"Pessoa Juridica");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}	
+	
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null ) {
+			return null;
+		}else {
+			for(TipoCliente tp : TipoCliente.values()) {
+				if(tp.equals(cod)) {
+					return tp;
+				}
+			}
+		}
+		
+		 throw new IllegalArgumentException("O codigo informado não é valido: "+cod);
+	}
+}
