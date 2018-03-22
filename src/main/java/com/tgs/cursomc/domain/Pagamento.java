7b7c306c751fdb67517a439243fe7ad2d49d601b
tiiiagoo <tiiiagoogs@gmail.com>
2018-03-22 14:55:13 -0300

    Atualização utilizando apenas @JsonIgnore

--- src/main/java/com/tgs/cursomc/domain/Pagamento.java
+++ src/main/java/com/tgs/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tgs.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -23,7 +23,7 @@ public abstract class Pagamento implements Serializable {
 	private Long id;
 	private int estadoPagamento;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId // Assume que o id do pedido é o mesmo que o id do pagamento
