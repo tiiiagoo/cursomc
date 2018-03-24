3890db143a8650f2643b7e6fc4275a9f86745ab5
tiiiagoo <tiiiagoogs@gmail.com>
2018-03-24 16:21:37 -0300

    inserindo uma nova Categoria com Post

--- src/main/java/com/tgs/cursomc/services/CategoriaService.java
+++ src/main/java/com/tgs/cursomc/services/CategoriaService.java
@@ -21,4 +21,9 @@ public class CategoriaService {
 		return categoria.orElseThrow(()-> new ObjectNotFoundException(
 							"Objeto não encontrado Id: "+ id +" , Tipo: "+ Categoria.class.getName()));
 	}
+
+	public Categoria insert(Categoria obj) {	
+		obj.setId(null);
+		return repository.save(obj);
+	}
 }
