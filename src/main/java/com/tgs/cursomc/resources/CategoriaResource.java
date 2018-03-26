5c878ecdd93b9e2c6c696373a43e6f91ca93b109
tiiiagoo <tiiiagoogs@gmail.com>
2018-03-26 00:35:50 -0300

    list categorias com DTO

--- src/main/java/com/tgs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tgs/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.tgs.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tgs.cursomc.domain.Categoria;
+import com.tgs.cursomc.dto.CategoriaDTO;
 import com.tgs.cursomc.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -27,6 +30,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria>  find(@PathVariable Long id) throws ObjectNotFoundException {
 		Categoria categoria = service.find(id);
 		return ResponseEntity.ok(categoria);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>>  findAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
