5ca8b7a79d80f37a9dc7fd9120eef6a5a917d87f
tiiiagoo <tiiiagoogs@gmail.com>
2018-04-02 23:44:40 -0300

    Subtotal e total do Pedido

--- src/main/java/com/tgs/cursomc/domain/Pedido.java
+++ src/main/java/com/tgs/cursomc/domain/Pedido.java
@@ -100,6 +100,14 @@ public class Pedido implements Serializable {
 	public void setItens(Set<ItemPedido> itens) {
 		this.itens = itens;
 	}
+	
+	public Double getValorTotal() {
+		double soma = 0.0;
+		for(ItemPedido i: itens) {
+			soma = soma +i.getSubTotal();
+		}
+		return soma;
+	}
 
 	@Override
 	public int hashCode() {
