e0a8a4b724b9c6413a44d6f2935541febe359db1
tiiiagoo <tiiiagoogs@gmail.com>
2018-04-09 17:34:59 -0300

    Implementando autorização

--- src/main/java/com/tgs/cursomc/config/SecurityConfig.java
+++ src/main/java/com/tgs/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.tgs.cursomc.security.JWTAuthenticationFilter;
+import com.tgs.cursomc.security.JWTAuthorizationFilter;
 import com.tgs.cursomc.security.JWTUtil;
 
 
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
