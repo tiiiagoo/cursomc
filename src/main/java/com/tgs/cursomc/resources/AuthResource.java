77ce4b5ebf8d000cabe36b699fbf042612683801
tiiiagoo <tiiiagoogs@gmail.com>
2018-04-16 17:16:57 -0300

    Refresh Token

---
+++ src/main/java/com/tgs/cursomc/resources/AuthResource.java
@@ -0,0 +1,32 @@
+package com.tgs.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tgs.cursomc.security.JWTUtil;
+import com.tgs.cursomc.security.UserSpringSecurity;
+import com.tgs.cursomc.services.UserService;
+
+
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value="/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+		UserSpringSecurity user = UserService.getAuthenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer "+token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
--- src/main/java/com/tgs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tgs/cursomc/resources/CategoriaResource.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,7 +39,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok(categoria);
 	}
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>>  findAll(){
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
@@ -55,8 +57,8 @@ public class CategoriaResource {
 		return ResponseEntity.ok(listPageDto);
 	}
 	
+	@PostMapping
 	@PreAuthorize("hasAnyRole('ADMIN')")
-	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO dto){
 		Categoria obj = service.fromDtoToObj(dto);
 		obj = service.insert(obj);
